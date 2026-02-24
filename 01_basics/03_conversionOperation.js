1044378f624be797332e44f15e6deea751bf738b
Priyanka <104508277+Priyanka28k@users.noreply.github.com>
2026-02-24 15:54:09 +0000

    conversion of datetypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "33"
+// let score = "33ab"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Priyanka"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  //  investigation state NAN
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+let isLoggedIn = "Priyanka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true, 0 => false
+// "" => false
+// "Priyanka" => true
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
