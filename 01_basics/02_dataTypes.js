24655c1d8595b6ba7efb1a60700d8ddad4ce696c
Priyanka <104508277+Priyanka28k@users.noreply.github.com>
2026-02-24 15:31:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)   // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3);    // code readability should be high
+
+console.log("Priyanka");
+
+let name = "Priyanka"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned the value
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof("Priyanka"));   // string
+console.log(typeof age);           // number
+console.log(typeof null);          // object
+console.log(typeof undefined);     // undefined
+
