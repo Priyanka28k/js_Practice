5c74a153858421574a5a3db15bd022f132393c07
Priyanka <104508277+Priyanka28k@users.noreply.github.com>
2026-02-26 15:29:31 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,3 +36,36 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+// *********************** Operations ************************* //
+
+console.log("*********OPERATIONS********");
+
+let value = 3;
+let negValue = - value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hi"
+let str2 = " Priyanka"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
