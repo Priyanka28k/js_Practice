3303927ff673aed5638400e1535f65f99de11626
Priyanka <104508277+Priyanka28k@users.noreply.github.com>
2026-02-26 16:11:12 +0000

    datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// this type of conversion can create confusion
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// === strictly check
+console.log("Strictly Check");
+
+console.log("2" == 2);
+console.log("2" === 2);
---
+++ dataTypes-summary.js
@@ -0,0 +1,8 @@
+// # Primitive Datatype
+// 7 Types : String, Number, boolean, Null, Undefined, Symbol, BigInt 
+
+
+
+// # Reference Type (Non Premitive Datatype)
+
+// Array, Objects, Functions
